5a105ec5e37729f5be7a9fec54c8d0e333060875
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-17 16:30:34 +0900

    freeeipa

--- install_functions/install_freeipa.sh
+++ install_functions/install_freeipa.sh
@@ -4,7 +4,7 @@ install_freeipa() {
 	check_cancel
 		zenity --auto-close &
 		(
-			echo $passwd | sudo -S apt install fly-admin-freeipa-server
+			echo $passwd | sudo -S apt install fly-admin-freeipa-server -y
 			# Проверка кода завершения
 			if [ $? -eq 0 ]; then
 				zenity --info --title="Успех" --text="Пакет успешно загружен!"
@@ -13,5 +13,22 @@ install_freeipa() {
 				exit 1
 			fi
 		) | zenity --progress --pulsate --title "Загрузка пакета" --text="Подождите, идет загрузка..." --auto-close
-	fly-term -e "fly-admin-freeipa-server"
+	fly-term -e "sudo fly-admin-freeipa-server"
+}
+
+connect_freeipa() {
+	passwd=$(zenity --password)
+	check_cancel
+		zenity --auto-close &
+		(
+			echo $passwd | sudo -S apt install astra-freeipa-client -y
+			# Проверка кода завершения
+			if [ $? -eq 0 ]; then
+				zenity --info --title="Успех" --text="Пакет успешно загружен!"
+			else
+				zenity --error --title="Ошибка" --text="Ошибка при загрузке пакета."
+				exit 1
+			fi
+		) | zenity --progress --pulsate --title "Загрузка пакета" --text="Подождите, идет загрузка..." --auto-close
+	fly-term -e "astra-freeipa-client -d slepsov.corp -p user123qwe -u admin -y"
 }
