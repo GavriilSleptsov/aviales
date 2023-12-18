2ad7f60589d57caa1cbc29c8dd445246645bba98
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-18 22:34:16 +0900

    Update install_network_folders.sh

--- install_functions/install_network_folders.sh
+++ install_functions/install_network_folders.sh
@@ -47,20 +47,20 @@ connect_network_folders () {
 	}
 	 check_checkbox() {
 		check_checkbox_number=$1
-		if network_folder$check_checkbox_number=TRUE; then
-			sed -i 's|#//192.168.0.2/$folder_mount_name /media/aviales/$folder_mount_name cifs username=tornado,password=torsys,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0|//192.168.0.2/$folder_mount_name /media/aviales/$folder_mount_name cifs username=tornado,password=torsys,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0|g' /etc/fstab
+		if [ "$network_folder$check_checkbox_number" == "TRUE"; then
+			echo $passwd | sudo -S sed -i 's|#//192.168.0.2/$folder_mount_name /media/aviales/$folder_mount_name cifs username=tornado,password=torsys,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0|//192.168.0.2/$folder_mount_name /media/aviales/$folder_mount_name cifs username=tornado,password=torsys,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0|g' /etc/fstab
 		else
-			sed -i 's|//192.168.0.2/$folder_mount_name /media/aviales/$folder_mount_name cifs username=tornado,password=torsys,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0|#//192.168.0.2/$folder_mount_name /media/aviales/$folder_mount_name cifs username=tornado,password=torsys,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0|g' /etc/fstab
+			echo $passwd | sudo -S sed -i 's|//192.168.0.2/$folder_mount_name /media/aviales/$folder_mount_name cifs username=tornado,password=torsys,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0|#//192.168.0.2/$folder_mount_name /media/aviales/$folder_mount_name cifs username=tornado,password=torsys,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0|g' /etc/fstab
 		fi
 	 }
 	
 	# Проверяем и добавляем программы в массив
 	if check_network_folder_connected "Users/"; then
 		network_folder1=TRUE
-		check_checkbox "1";
+		check_checkbox "1"
 	else
 		network_folder1=FALSE
-		check_checkbox "1";
+		check_checkbox "1"
 	fi
 	
 	zenity --list \
@@ -68,4 +68,12 @@ connect_network_folders () {
 	--column "Выберите" \
 	--column "Общие папки" \
 	$network_folder1 Users
+	
+	if check_network_folder_connected "Users/"; then
+		network_folder1=TRUE
+		check_checkbox "1"
+	else
+		network_folder1=FALSE
+		check_checkbox "1"
+	fi
 }
