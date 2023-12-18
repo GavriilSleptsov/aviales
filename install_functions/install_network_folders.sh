5d2db792cb98fa3596c907b0e97c2223bade67a4
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-18 19:49:25 +0900

    Update install_network_folders.sh

--- install_functions/install_network_folders.sh
+++ install_functions/install_network_folders.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-connect_folders() {
-credentials_network_folder="cifs username=tornado,password=torsys,iocharset=utf8,file_mode=0777,dir_mode=0777 0 0"
-# Строка, которую вы ищете в /etc/fstab
-search_string="//192.168.0.2/Users /media/test $credentials_network_folder"
-
-# Строка из /etc/fstab
-fstab_line=$(grep "$search_string" /etc/fstab)
-
-# Проверяем наличие строки в /etc/fstab
-if [[ "$fstab_line" == "$search_string" ]]; then
-    echo "true"
-else
-    echo "false"
-fi
-}
+zenity --list \
+      --checklist \
+      --column "checkbox" \
+      --column "Player" \
+      --column "Club" \
+      --column "Country" \
+      FALSE Messi PSG Argentina \
+      FALSE Neymar PSG Brazil \
+      FALSE Ronaldo Manu Portugal
