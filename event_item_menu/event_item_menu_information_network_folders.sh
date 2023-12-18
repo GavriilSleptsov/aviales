cabd2d9a76980ae94a7d9f11adb99e88c3fd5c87
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-18 19:53:56 +0900

    шара

--- event_item_menu/event_item_menu_information_network_folders.sh
+++ event_item_menu/event_item_menu_information_network_folders.sh
@@ -2,5 +2,5 @@
 #---------------init event for item_menu_information_network_folders-------------------------------
 declare -A event_menu
 event_menu["$item_menu_information_network_folders"]="run_menu ${item_menu_information_network_folders_all[@]}"
-event_menu["$item_menu_information_network_folders_connect"]="connect_folders"
+event_menu["$item_menu_information_network_folders_connect"]="connect_network_folders"
 event_menu["$item_menu_information_network_folders_disconnect"]="disconnect_folders"
--- install_functions/install_network_folders.sh
+++ install_functions/install_network_folders.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-zenity --list \
-      --checklist \
-      --column "checkbox" \
-      --column "Player" \
-      --column "Club" \
-      --column "Country" \
-      FALSE Messi PSG Argentina \
-      FALSE Neymar PSG Brazil \
-      FALSE Ronaldo Manu Portugal
+connect_network_folders () {
+	zenity --list \
+    --checklist \
+    --column "checkbox" \
+    --column "Player" \
+    --column "Club" \
+    --column "Country" \
+    FALSE Messi PSG Argentina \
+    FALSE Neymar PSG Brazil \
+    FALSE Ronaldo Manu Portugal
+}
