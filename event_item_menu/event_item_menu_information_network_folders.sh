9513847e31cea5c4383bcf1cb3581c4abc5d9605
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-18 19:40:51 +0900

    шара!

--- event_item_menu/event_item_menu_information_network_folders.sh
+++ event_item_menu/event_item_menu_information_network_folders.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#---------------init event for item_menu_information_freeipa-------------------------------
+#---------------init event for item_menu_information_network_folders-------------------------------
 declare -A event_menu
 event_menu["$item_menu_information_network_folders"]="run_menu ${item_menu_information_network_folders_all[@]}"
 event_menu["$item_menu_information_network_folders_connect"]="connect_folders"
