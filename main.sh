6f3082788e3d02e53cb1d5b28e785bc246a559f2
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-20 00:59:23 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -181,6 +181,12 @@ remove_app(){
     local passwd=$(zenity --password)
     check_cancel
     echo $passwd | sudo -S apt remove "$1" -y
+	if [ $? -eq 0 ]; then
+		zenity --info --title="Успех" --text="Файл успешно удален!"
+		else
+			zenity --error --title="Ошибка" --text="Ошибка при удалении файла!."
+		exit 1
+	fi
 }
 
 #-------------------------------------domain menu------------------------------------#
