6cd9346f7566692f80c72991363c35b9b82b0e18
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-20 03:59:47 +0900

    FOLDER

--- install_functions/install_network_folders.sh
+++ install_functions/install_network_folders.sh
@@ -11,7 +11,7 @@ connect_network_folders () {
 	
     echo "$passwd" | sudo -S sh -c 'cat <<EOL >> /etc/fstab
 #
-# -- Общие документы --
+# -- E-mail --
 #//192.168.0.202/E-mail /media/aviales/E-mail cifs username=tornado,password=torsys,iocharset=utf8,nofail,_netdev,file_mode=0777,dir_mode=0777 0 0
 #
 # -- Архив баз 1С
