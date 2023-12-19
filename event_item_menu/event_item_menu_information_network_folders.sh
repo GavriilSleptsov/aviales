356814931b8ac5e07d25f857bc5f9e4531848965
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-20 04:09:32 +0900

    FOLDER

--- event_item_menu/event_item_menu_information_network_folders.sh
+++ event_item_menu/event_item_menu_information_network_folders.sh
@@ -3,4 +3,3 @@
 declare -A event_menu
 event_menu["$item_menu_information_network_folders"]="run_menu ${item_menu_information_network_folders_all[@]}"
 event_menu["$item_menu_information_network_folders_connect"]="connect_network_folders"
-event_menu["$item_menu_information_network_folders_disconnect"]="disconnect_folders"
--- item_menu/item_menu_information_network_folders.sh
+++ item_menu/item_menu_information_network_folders.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-export item_menu_information_network_folders_connect="Подключить сетевую шару (выборочно)"
-export item_menu_information_network_folders_disconnect="Удалить сетевую шару (выборочно)"
+export item_menu_information_network_folders_connect="Подключить данные сетевых шар"
 export item_menu_information_network_folders_all=(
-"\"$item_menu_information_network_folders_connect\"" 
-"\"$item_menu_information_network_folders_disconnect\"" 
+"\"$item_menu_information_network_folders_connect\""  
 "\"$exit_menu\"" 
 "\"$exit_app\"")
