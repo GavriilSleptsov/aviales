b8e5f70f95123e777ff55eb1adc2ffddeca3b8ed
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-19 13:04:33 +0900

    Update install_network_folders.sh

--- install_functions/install_network_folders.sh
+++ install_functions/install_network_folders.sh
@@ -6,9 +6,9 @@ connect_network_folders () {
         --add-password="Пароль")
     
     check_cancel
-
+	echo "$passwd" | sudo -S mkdir -p /media/aviales/Общие\ документы
     network_folder_text=$(curl "https://raw.githubusercontent.com/GavriilSleptsov/aviales/main/text_for_zenity/network_folder")
-
+	
     echo "$passwd" | sudo -S sh -c 'cat <<EOL >> /etc/fstab
 # -- Общие документы --
 #//192.168.0.200/Общие\040документы /media/aviales/Общие\040документы cifs username=tornado,password=torsys,iocharset=utf8,nofail,_netdev,file_mode=0777,dir_mode=0777 0 0
