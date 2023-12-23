254c245b775dc3092c5e52dd5a053baab473087f
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-23 13:28:56 +0900

    Update event_item_menu_information_repo.sh

--- event_item_menu/event_item_menu_information_repo.sh
+++ event_item_menu/event_item_menu_information_repo.sh
@@ -5,5 +5,5 @@ event_menu["$item_menu_information_repo"]="run_menu ${item_menu_information_repo
 event_menu["$item_menu_information_repo_rcit"]="repo_rcit"
 event_menu["$item_menu_information_repo_frozen1_7_3"]="repo_frozen1_7_3"
 event_menu["$item_menu_information_repo_frozen1_7_4"]="repo_frozen1_7_4"
-event_menu["$item_menu_information_repo_frozen1_7_4"]="repo_frozen1_7_5"
+event_menu["$item_menu_information_repo_frozen1_7_5"]="repo_frozen1_7_5"
 
