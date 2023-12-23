ae4ebce94b5da3c58c3616c7722192ff4a3ca1af
GavriilSleptsov <killerjikey84@gmail.com>
2023-12-23 13:28:15 +0900

    Update item_menu_information_repo.sh

--- item_menu/item_menu_information_repo.sh
+++ item_menu/item_menu_information_repo.sh
@@ -3,10 +3,12 @@
 export item_menu_information_repo_rcit="Добавить сетевые репозитории РЦИТ"
 export item_menu_information_repo_frozen1_7_3="Добавить репозитории frozen 1.7.3"
 export item_menu_information_repo_frozen1_7_4="Добавить репозитории frozen 1.7.4"
+export item_menu_information_repo_frozen1_7_5="Добавить репозитории frozen 1.7.5"
 export item_menu_information_repo_info="Описание репозиториев"
 export item_menu_information_repo_all=(
 "\"$item_menu_information_repo_rcit\"" 
 "\"$item_menu_information_repo_frozen1_7_3\""  
 "\"$item_menu_information_repo_frozen1_7_4\""
+"\"$item_menu_information_repo_frozen1_7_5\""
 "\"$exit_menu\"" 
 "\"$exit_app\"")
