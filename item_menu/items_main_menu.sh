b75f74f86c53341d8d08a3796bdda727428d4783
GavriilSleptsov <killerjikey84@gmail.com>
2024-01-17 20:14:51 +0900

    Update items_main_menu.sh

--- item_menu/items_main_menu.sh
+++ item_menu/items_main_menu.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #---------------init items_main_menu-------------------------------
-if getent group | grep -qw 'astra-admin' && id -nG | grep -qw 'astra-admin'; then
-	export item_menu_information_domen="Подключение к домену aviales.local"
-	export item_menu_information_resources="Информационные ресурсы"
-	export item_menu_information_install="Установка программ"
-	export item_menu_information_remove="Удаление программ"
-	export item_menu_information_repo="Сетевые репозитории"
-	export item_menu_information_pomogator="О программе"
-	export item_menu_information_network_folders="Сетевые папки"
-	export item_menu_information_cryptopro="КриптоПро 5"
-	export item_menu_information_one_c="1С: Предприятие. Тонкий Клиент"
-	export item_menu_information_netmgr="Настройка сетевого интерфейса (eth0)"
-	export items_main_menu=( 
+export item_menu_information_domen="Подключение к домену aviales.local"
+export item_menu_information_resources="Информационные ресурсы"
+export item_menu_information_install="Установка программ"
+export item_menu_information_remove="Удаление программ"
+export item_menu_information_repo="Сетевые репозитории"
+export item_menu_information_pomogator="О программе"
+export item_menu_information_network_folders="Сетевые папки"
+export item_menu_information_cryptopro="КриптоПро 5"
+export item_menu_information_one_c="1С: Предприятие. Тонкий Клиент"
+export item_menu_information_netmgr="Настройка сетевого интерфейса (eth0)"
+export items_main_menu=( 
 	"$item_menu_information_netmgr"
 	"$item_menu_information_domen" 
 	"$item_menu_information_resources"  
@@ -24,15 +23,3 @@ if getent group | grep -qw 'astra-admin' && id -nG | grep -qw 'astra-admin'; the
 	"$item_menu_information_pomogator" 
 	"$exit_app")
 	
-else
-	export item_menu_information_resources="Информационные ресурсы"
-	export item_menu_information_pomogator="О программе"
-	export item_menu_information_network_folders="Сетевые папки"
-	export items_main_menu=( 
-	"$item_menu_information_resources"  
-	"$item_menu_information_pomogator" 
-	"$item_menu_information_network_folders"
-	"$exit_app")
-fi
-
-
