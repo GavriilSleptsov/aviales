c7d09a20555a69431d40b14b3f573890d295a3c8
GavriilSleptsov <killerjikey84@gmail.com>
2024-01-17 20:10:13 +0900

    Update netmgr_network.sh

--- install_functions/netmgr_network.sh
+++ install_functions/netmgr_network.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
 add_network() {
-	имя активного соединения
-	con_name=$(nmcli --fields NAME -t connection show --active)
+	#имя активного соединения
+	#con_name=$(nmcli --fields NAME -t connection show --active)
 
-	if [ -z "$con_name" ] ; then echo "Нет активных соединений" && exit 0;
-	fi
-	echo "$con_name"
+	#if [ -z "$con_name" ] ; then echo "Нет активных соединений" && exit 0;
+	#fi
+	#echo "$con_name"
 
 	#сбор данных
-	add_network_config
-	nmcli connection modify "$con_name" connection.autoconnect yes ipv4.method manual ipv4.dns $dns1_ip ipv4.dns-search $search_domain_name ipv4.addresses $computer_ip/$subnet_mask ipv4.gateway $gateway_ip
+	#add_network_config
+	#nmcli connection modify "$con_name" connection.autoconnect yes ipv4.method manual ipv4.dns $dns1_ip ipv4.dns-search $search_domain_name ipv4.addresses $computer_ip/$subnet_mask ipv4.gateway $gateway_ip
 
-	nmcli connection down "$con_name"
-	nmcli connection up "$con_name"
-	sleep 5
-}
-
- form_data=$(zenity --forms --title="Введите данные" --text="Введите данные:" \
+	#nmcli connection down "$con_name"
+	#nmcli connection up "$con_name"
+	#sleep 5
+	 form_data=$(zenity --forms --title="Введите данные" --text="Введите данные:" \
             --add-entry="Введите IP адрес ПК" \
             --add-entry="Введите маску подсети в виде: 24" \
             --add-entry="Введите IP DNS сервера" \
@@ -29,3 +27,5 @@ add_network() {
             dns_server=$(echo "$form_data" | awk -F '|' '{print $3}')
 			gateway=$(echo "$form_data" | awk -F '|' '{print $4}')
 			search_domain=$(echo "$form_data" | awk -F '|' '{print $5}')
+}
+
