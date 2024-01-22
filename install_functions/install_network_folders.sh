c1f5a66f3289038b9641f7c8558f6cd50db0ac45
GavriilSleptsov <killerjikey84@gmail.com>
2024-01-22 17:11:46 +0900

    Update install_network_folders.sh

--- install_functions/install_network_folders.sh
+++ install_functions/install_network_folders.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+create_folder_for_network_folders () {
+	if [ -d /etc/skel/Desktop/Сетевые\ папки ]; then
+		echo "Папка уже существует. Ничего не нужно делать."
+	else
+		mkdir /etc/skel/Desktop/Сетевые\ папки
+	fi
+}
+
 connect_network_folders () {
 	#!/bin/bash
 
@@ -16,28 +24,25 @@ connect_network_folders () {
 
 	# Проверяем, были ли выбраны какие-либо опции
 	if [ -n "$selected_options" ]; then
-	# Преобразуем строку с выбранными опциями в массив
-	IFS=':' read -ra options_array <<< "$selected_options"
-
-	# Обрабатываем каждую выбранную опцию
-	for option in "${options_array[@]}"; do
-		case $option in
-			"sharetest")
-				echo "Выбрана Опция 1"
-				;;
-			"folders")
-				echo "Выбрана Опция 2"
-				;;
-			"gto")
-				echo "Выбрана Опция 3"
-				;;
-			*)
-				# Обработка для других опций (если есть)
-				;;
-		esac
-	done
-	else
-	# Если ничего не выбрано
-		echo "Вы не выбрали ни одной опции."
+		credentials_for_netfolders="cifs,user=admin,password=P@ssw0rd,iocharset=utf8,dir_mode=0777,file_mode=0777,_netdev,nofail 0 0"
+		create_folder_for_network_folders
+		IFS=':' read -ra options_array <<< "$selected_options"
+		for option in "${options_array[@]}"; do
+			case $option in
+				"sharetest")
+					mkdir -p /media/yakutia/sharetest
+					echo "//192.168.0.200/sharetest /media/yakutia/sharetest $credentials_for_netfolders"
+					;;
+				"folders")
+					echo "Выбрана Опция 2"
+					;;
+				"gto")
+					echo "Выбрана Опция 3"
+					;;
+				*)
+					# Обработка для других опций (если есть)
+					;;
+			esac
+		done
 	fi
 }
