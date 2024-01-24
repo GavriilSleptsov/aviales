fb0f4075a310d9e92ebbef5910a9f388d250b125
GavriilSleptsov <killerjikey84@gmail.com>
2024-01-24 17:34:58 +0900

    Update install_network_folders.sh

--- install_functions/install_network_folders.sh
+++ install_functions/install_network_folders.sh
@@ -14,19 +14,20 @@ connect_network_folders () {
 	# Проверяем, были ли выбраны какие-либо шары
 	if [ -n "$selected_options" ]; then
 		passwd=$(zenity --password)
-		if [ -d /etc/skel/Desktop/Сетевые\ папки ]; then
+		if [ -d "/etc/skel/Desktop/Сетевые\ папки" ]; then
 			echo "Папка уже существует. Ничего не нужно делать."
 		else
 			echo $passwd | sudo mkdir /etc/skel/Desktop/Сетевые\ папки
 		fi
 		credentials_for_netfolders="cifs,user=admin,password=P@ssw0rd,iocharset=utf8,dir_mode=0777,file_mode=0777,_netdev,nofail 0 0"
-		create_folder_for_network_folders
 		IFS=':' read -ra options_array <<< "$selected_options"
 		for option in "${options_array[@]}"; do
 			case $option in
 				"sharetest")
 					echo $passwd | sudo mkdir -p /media/yakutia/sharetest
-					echo $passwd | sudo echo "//192.168.0.200/sharetest /media/yakutia/sharetest $credentials_for_netfolders" >> /etc/fstab
+					echo $passwd | sudo -S tee -a  "/etc/fstab" > /dev/null <<EOF
+//192.168.0.200/sharetest /media/yakutia/sharetest $credentials_for_netfolders
+EOF
 					;;
 				"folders")
 					echo "Выбрана Опция 2"
