e8134090a9aa4d0b028a33882fcd5d02276caa01
GavriilSleptsov <killerjikey84@gmail.com>
2024-01-24 18:29:11 +0900

    Update install_network_folders.sh

--- install_functions/install_network_folders.sh
+++ install_functions/install_network_folders.sh
@@ -18,16 +18,25 @@ connect_network_folders () {
 			echo "Папка уже существует. Ничего не нужно делать."
 		else
 			echo $passwd | sudo -S mkdir /etc/skel/Desktop/Сетевые\ папки
+			echo $passwd | sudo -S tee -a "/etc/skel/Desktop/Сетевые папки/.directory" <<EOF
+[Desktop Entry]
+Type=Directory
+NoDisplay=false
+Icon=prefences-system-network-sharing
+Hidden=false
+EOF
+			
 		fi
 		credentials_for_netfolders="cifs,user=admin,password=P@ssw0rd,iocharset=utf8,dir_mode=0777,file_mode=0777,_netdev,nofail 0 0"
 		IFS=':' read -ra options_array <<< "$selected_options"
 		for option in "${options_array[@]}"; do
 			case $option in
 				"sharetest")
-					echo $passwd | sudo mkdir -p /media/yakutia/sharetest
+					echo $passwd | sudo  mkdir -p /media/yakutia/sharetest
 					echo $passwd | sudo -S tee -a "/etc/fstab" <<EOF
 //192.168.0.200/sharetest /media/yakutia/sharetest $credentials_for_netfolders
 EOF
+					echo $passwd | sudo -S ln -s /media/yakutia/sharetest "/etc/skel/Desktop/Сетевые папки"
 					;;
 				"folders")
 					echo "Выбрана Опция 2"
